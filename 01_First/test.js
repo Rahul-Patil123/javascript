84990d5a3281abfd0e486cd77220dac206b7a7d8
Rahul-Patil123 <141249410+Rahul-Patil123@users.noreply.github.com>
2024-07-20 17:05:30 +0000

    datatypes

--- 01_First/test.js
+++ 01_First/test.js
@@ -1,1 +1,13 @@
+"use strict";
 console.log("Hell World")
+//Datatypes
+//number
+//bigint
+//string
+//null
+//undefined
+//boolean
+//symbol
+//object
+//null is a object
+//undefined is undefined datatype
